e86fbd4bd430a754af66a7c478f99799d3a41ff1
josep <josep@MSI>
2024-03-19 17:55:03 +0530

    sample

---
+++ Sample1/src/main/java/example/Sum.java
@@ -0,0 +1,18 @@
+package example;
+
+public class Sum {
+	int a=10;
+	int b=42;
+	public void add()
+	{
+		int c=a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Sum s=new Sum();
+        s.add();
+	}
+
+}
