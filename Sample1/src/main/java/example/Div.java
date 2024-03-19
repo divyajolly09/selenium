4ef7c650ff73067a2ad41da9403a98502e6ae998
josep <josep@MSI>
2024-03-19 18:28:06 +0530

    hii

---
+++ Sample1/src/main/java/example/Div.java
@@ -0,0 +1,18 @@
+package example;
+
+public class Div {
+	int a=10;
+	int b=2;
+	public void div()
+	{
+		int c=a/b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Div s=new Div();
+        s.div();
+	}
+
+}
